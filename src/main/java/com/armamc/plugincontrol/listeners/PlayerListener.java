73d11470dc197bbd3f2cc3caa0733909dd3ac4d8
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-08 14:28:49 -0300

    use new ConfigManager class

--- src/main/java/com/armamc/plugincontrol/listeners/PlayerListener.java
+++ src/main/java/com/armamc/plugincontrol/listeners/PlayerListener.java
@@ -1,21 +1,20 @@
 package com.armamc.plugincontrol.listeners;
 
 import com.armamc.plugincontrol.PluginControl;
-import com.armamc.plugincontrol.config.Config;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import com.armamc.plugincontrol.config.ConfigManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
 
 public class PlayerListener implements Listener {
     private final PluginControl plugin;
-    private final Config config;
+    private final ConfigManager config;
     private final Permission bypass;
 
-    public PlayerListener(PluginControl plugin) {
+    public PlayerListener(@NotNull PluginControl plugin) {
         this.plugin = plugin;
         this.config = plugin.getPluginConfig();
         this.bypass = new Permission("plugincontrol.bypass");
@@ -23,16 +22,16 @@ public class PlayerListener implements Listener {
 
     public void init() {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        plugin.getServer().getOnlinePlayers().stream()
-                .filter(player -> !player.hasPermission(bypass))
-                .forEach(player -> player.kickPlayer(config.serialize(config.getKickMessage())));
+        var onlinePlayers = plugin.getServer().getOnlinePlayers();
+        for (var players : onlinePlayers) {
+            if (players.hasPermission(bypass)) continue;
+            players.kickPlayer(config.serialize(config.getKickMessage()));
+        }
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    private void onPlayerLogin(PlayerLoginEvent event) {
-        if (event.getPlayer().hasPermission(bypass)) {
-            return;
-        }
+    private void onPlayerLogin(@NotNull PlayerLoginEvent event) {
+        if (event.getPlayer().hasPermission(bypass)) return;
         event.disallow(PlayerLoginEvent.Result.KICK_OTHER, config.serialize(config.getKickMessage()));
     }
 
