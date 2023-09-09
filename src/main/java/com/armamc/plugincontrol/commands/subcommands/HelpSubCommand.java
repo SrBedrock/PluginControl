31ae4233f52f70ba4bede100fd6f459655787069
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-09 12:53:59 -0300

    update help subcommand

--- src/main/java/com/armamc/plugincontrol/commands/subcommands/HelpSubCommand.java
+++ src/main/java/com/armamc/plugincontrol/commands/subcommands/HelpSubCommand.java
@@ -1,26 +1,31 @@
 package com.armamc.plugincontrol.commands.subcommands;
 
 import com.armamc.plugincontrol.PluginControl;
+import com.armamc.plugincontrol.managers.MessageManager;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class HelpSubCommand implements SubCommand {
-    @Contract(pure = true)
-    public HelpSubCommand(PluginControl plugin) {
+    private final MessageManager message;
 
+    @Contract(pure = true)
+    public HelpSubCommand(@NotNull PluginControl plugin) {
+        this.message = plugin.getMessageManager();
     }
 
     @Override
     public void execute(CommandSender sender, Command command, String label, String[] args) {
-
+        message.send(sender, message.getHelpList(), Placeholder.parsed("command", label));
     }
 
     @Override
     public List<String> tabComplete(CommandSender sender, Command command, String label, String[] args) {
-        return null;
+        return List.of();
     }
 
 }
