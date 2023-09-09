a3f46048f9d1da33449ff46c51b980798ed77d22
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-09 12:58:44 -0300

    update toggle subcommand

--- src/main/java/com/armamc/plugincontrol/commands/subcommands/ToggleSubCommand.java
+++ src/main/java/com/armamc/plugincontrol/commands/subcommands/ToggleSubCommand.java
@@ -1,26 +1,38 @@
 package com.armamc.plugincontrol.commands.subcommands;
 
 import com.armamc.plugincontrol.PluginControl;
+import com.armamc.plugincontrol.managers.ConfigManager;
+import com.armamc.plugincontrol.managers.MessageManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class ToggleSubCommand implements SubCommand {
-    @Contract(pure = true)
-    public ToggleSubCommand(PluginControl plugin) {
+    private final ConfigManager config;
+    private final MessageManager message;
 
+    @Contract(pure = true)
+    public ToggleSubCommand(@NotNull PluginControl plugin) {
+        this.config = plugin.getConfigManager();
+        this.message = plugin.getMessageManager();
     }
 
     @Override
     public void execute(CommandSender sender, Command command, String label, String[] args) {
-
+        config.setEnabled(!config.isEnabled());
+        if (config.isEnabled()) {
+            message.send(sender, message.getPluginEnabled());
+        } else {
+            message.send(sender, message.getPluginDisabled());
+        }
     }
 
     @Override
     public List<String> tabComplete(CommandSender sender, Command command, String label, String[] args) {
-        return null;
+        return List.of();
     }
 
 }
