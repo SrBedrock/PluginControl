68e40565e28a2a8ffb8de2ff1d3dfb1354e6202f
ThiagoROX <51332006+SrBedrock@users.noreply.github.com>
2023-09-29 19:20:19 -0300

    add feedeback to /pc check

--- src/main/java/com/armamc/plugincontrol/commands/subcommands/CheckSubCommand.java
+++ src/main/java/com/armamc/plugincontrol/commands/subcommands/CheckSubCommand.java
@@ -1,6 +1,8 @@
 package com.armamc.plugincontrol.commands.subcommands;
 
 import com.armamc.plugincontrol.PluginControl;
+import com.armamc.plugincontrol.managers.ConfigManager;
+import com.armamc.plugincontrol.managers.MessageManager;
 import com.armamc.plugincontrol.managers.PluginsManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,16 +12,24 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class CheckSubCommand implements SubCommand {
+    private final ConfigManager config;
+    private final MessageManager message;
     private final PluginsManager manager;
 
     @Contract(pure = true)
     public CheckSubCommand(@NotNull PluginControl plugin) {
+        this.config = plugin.getConfigManager();
+        this.message = plugin.getMessageManager();
         this.manager = plugin.getPluginsManager();
     }
 
     @Override
     public void execute(CommandSender sender, Command command, String label, String @NotNull [] args) {
-        manager.checkPlugins();
+        if (!config.isEnabled()) {
+            message.send(sender, message.getCheckingDisabled());
+        } else {
+            manager.checkPlugins();
+        }
     }
 
     @Override
