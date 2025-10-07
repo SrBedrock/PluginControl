ee047b58b385967b41b76e37b5114cf1e7fbcb40
ThiagoROX <thiagokenis@live.com>
2025-10-06 23:49:22 -0300

    Refactor variable declarations to use 'final' and update annotations
    Changed variable declarations in PluginControl.java to use 'final var' for improved immutability and clarity. Replaced usage of @NonNull from Checker Framework with @NotNull from JetBrains annotations for consistency. No functional changes were made.

--- src/main/java/com/armamc/plugincontrol/PluginControl.java
+++ src/main/java/com/armamc/plugincontrol/PluginControl.java
@@ -8,8 +8,8 @@ import com.technicjelle.UpdateChecker;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public final class PluginControl extends JavaPlugin {
     private BukkitAudiences adventure;
@@ -45,8 +45,8 @@ public final class PluginControl extends JavaPlugin {
     }
 
     private void registerCommands() {
-        var command = new MainCommand(this);
-        var pluginCommand = getCommand("plugincontrol");
+        final var command = new MainCommand(this);
+        final var pluginCommand = getCommand("plugincontrol");
         if (pluginCommand != null) {
             pluginCommand.setExecutor(command);
             pluginCommand.setTabCompleter(command);
@@ -59,7 +59,7 @@ public final class PluginControl extends JavaPlugin {
 
     private void checkUpdate() {
         if (configManager.isUpdateNotifierEnabled()) {
-            UpdateChecker updateChecker = new UpdateChecker("SrBedrock", "PluginControl", "1.2.1");
+            final UpdateChecker updateChecker = new UpdateChecker("SrBedrock", "PluginControl", "1.2.1");
             updateChecker.checkAsync();
             updateChecker.logUpdateMessage(getLogger());
         }
@@ -81,15 +81,15 @@ public final class PluginControl extends JavaPlugin {
     }
 
     @Contract(pure = true)
-    public @NonNull BukkitAudiences adventure() {
+    public @NotNull BukkitAudiences adventure() {
         if (this.adventure == null) {
             throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
         }
         return this.adventure;
     }
 
-    public boolean isPluginEnabled(String pluginName) {
-        var plugin = getServer().getPluginManager().getPlugin(pluginName);
+    public boolean isPluginEnabled(final String pluginName) {
+        final var plugin = getServer().getPluginManager().getPlugin(pluginName);
         return plugin != null && plugin.isEnabled();
     }
 
